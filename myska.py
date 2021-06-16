e9d8947739d8a96f3c8112f7c371c315466a6fc0
Petr Stádník <donvonhill@gmail.com>
2021-06-16 15:18:25 +0200

    sound with beepy helps with working on all platforms

--- myska.py
+++ myska.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
-import os
 from pynput.mouse import Button, Controller
+import beepy
 
 mouse = Controller()
 
@@ -9,11 +9,12 @@ while (True):
     # datetime object containing current date and time
     now = datetime.now()
     print(now, "Pressing left button on mouse to stay awake")
+    beepy.beep(sound="ping")
     #os.system('say "moving"') #say moving
-    os.system('afplay /System/Library/Sounds/Morse.aiff') #play system sound Tink
+    #os.system('afplay /System/Library/Sounds/Morse.aiff') #play system sound Tink
     
     # Move pointer relative to current position
-    #mouse.move(5, 0)
+    #mouse.moe(5, 0)
     #time.sleep(0.2)
     #mouse.move(-5, 0)
     
